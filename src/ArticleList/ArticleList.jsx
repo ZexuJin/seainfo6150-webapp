a892abb966ace63d3f9f7cd683330e22ed594810
Zexu Jin <54969533+ZexuJin@users.noreply.github.com>
2019-11-06 13:49:49 -0800

    Add files via upload

---
+++ src/ArticleList/ArticleList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import ArticleListItems from './ArticleListItems';
+import styles from './ArticleList.module.css';
+
+const ArticleList = props => {
+  return (
+      <div className={ArticleList}>
+              <ul className={styles.articleList}>
+                  {props.articles.map(article => (
+                    <li key={article.slug}>
+                      <ArticleListItems title={article.title}
+                                        pubDate={article.pubDate}
+                                        shortText={article.shortText}
+                                        slug = {article.slug}
+                                        author = {article.author}
+                                        url = {article.image._url}/>
+                    </li>
+                  ))}
+              </ul>
+      </div>
+  );
+};
+
+ArticleList.propTypes = {
+  articles: PropTypes.array.isRequired
+};
+export default ArticleList;
